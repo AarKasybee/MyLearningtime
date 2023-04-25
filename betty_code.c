0e5ecca3bfcf522f1501d5e61df99abd18ef84cf
Biakes <akinshiloabimbola@gmail.com>
2023-04-24 23:39:44 -0700

    beautiful betty task 0

---
+++ betty_code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a beautiful betty check.
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 12; b < 10; b++)
+	{
+		printf("Beautiful betty checks\n");
+	}
+	return (0);
+}
